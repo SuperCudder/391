f5d11f921659661aef40f823ca6aef43596097e9
SuperCudder <144271447+SuperCudder@users.noreply.github.com>
2025-09-11 13:28:39 -0400

    various implementations for the different internal pages, beginning work on calculator

---
+++ projects/mini-projects/mp1/calculator.js
@@ -0,0 +1,17 @@
+add(a, b)
+    return a + b
+
+subtract(a, b)
+    return a - b
+
+divide(a, b)
+    return a / b
+
+multiply(a, b)
+    return a * b
+
+exponent(a, b)
+ for (int i = 0; i < b; i++) {
+     c = a * b
+}
+ return c
